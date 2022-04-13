a8746c03c931aec7bafe8d002ca9ac1c9cc4d27f
Lance <lanceyu619@gmail.com>
2022-04-13 04:44:50 +0000

    heh

---
+++ lighthouse-js-fundamentals/ingredients.js
@@ -0,0 +1,14 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var x = 0
+while(x < ingredients.length){
+  console.log(ingredients[x]);
+  x++;
+}
+
+for (var i=0; i<ingredients.length;i++){
+  console.log(ingredients[i]);
+}
+
+for (var i=ingredients.length-1; i>-1;i--){
+  console.log(ingredients[i])
+}
